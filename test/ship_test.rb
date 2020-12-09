2ba177fdccc95a0e37da21e7c3c9321b2b214d0e
Jeremiah Michlitsch <jeremiahmichlitsch@gmail.com>
2020-12-09 16:46:44 -0700

    Refactor the ship test file

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -3,39 +3,35 @@ require 'minitest/pride'
 require './lib/ship'
 
 class ShipTest < Minitest::Test
-
+  def setup
+    @ship = Ship.new("Cruiser", 3)
+  end
   def test_ship_exists
-    ship = Ship.new("Cruiser", 3)
 
-    assert_instance_of Ship, ship
+    assert_instance_of Ship, @ship
   end
 
   def test_ship_has_attributes
-    ship = Ship.new("Cruiser", 3)
 
-    assert_equal ship.name, "Cruiser"
-    assert_equal ship.length, 3
+    assert_equal @ship.name, "Cruiser"
+    assert_equal @ship.length, 3
   end
 
   def test_ship_has_health
-    ship = Ship.new("Cruiser", 3)
 
-    assert_equal ship.health, 3
+    assert_equal @ship.health, 3
   end
 
   def test_ship_can_take_hits
-    ship = Ship.new("Cruiser", 3)
-    ship.hit
+    @ship.hit
 
-    assert_equal ship.health , 2
+    assert_equal @ship.health , 2
   end
 
   def test_ship_can_sink
-    ship = Ship.new("Cruiser", 3)
     3.times do |hit|
-      ship.hit
+      @ship.hit
     end
-
-    assert_equal ship.sunk?, true
+    assert_equal @ship.sunk?, true
   end
 end
