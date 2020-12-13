f598864ab5c12dfb62fc83fdac428c133de9ead3
Andrew Smith <andrewpatrick138@gmail.com>
2020-12-13 13:08:04 -0800

    initial iteration 3 commit

---
+++ lib/setup.rb
@@ -0,0 +1,12 @@
+class Setup
+  attr_reader :player,
+              :computer,
+              :player_board,
+              :computer_board
+
+  def initialize 
+    @player = Player.new
+    @computer = Computer.new
+    @player_board = Board.new
+    @computer_board = Board.new
+  end
